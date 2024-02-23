96c483d957a68a6e0bb0b02bc290255d5d8d76b1
Isaac <isaac@snibi.com>
2024-02-23 16:50:28 +1000

    🗃️ [graphql] working sub-folder and sub-files queries

--- src/gql.ts
+++ src/gql.ts
@@ -1,6 +1,7 @@
 import { buildSchema } from 'graphql';
 import { createHandler } from 'graphql-http/lib/use/express';
 import { Folder } from './models/folder';
+import { File } from './models/file';
 
 const schema = buildSchema(/*GraphQL */ `
     type Query {
@@ -11,19 +12,27 @@ const schema = buildSchema(/*GraphQL */ `
         id: ID!
         name: String!
         parentId: ID
+        subFolders: [Folder!]!
+        files: [File!]!
+    }
+    
+    type File {
+      id: ID!
+      name: String!
+      folderId: ID!
     }
 
 `);
 
 const root = {
   folder: async (params) => {
-    const folder = await Folder.findOne({ where: { id: params.id } });
-    return folder.toJSON();
+    const data = await getFolder(params.id);
+    data.subFolders = await subFolders(params.id);
+    data.files = await subFiles(params.id);
+    return data;
   },
   folders: async (params) => {
-    const parentId = params.parentId ?? 1;
-    const folders = await Folder.findAll({ where: { parentId } });
-    return folders.map((f) => f.toJSON());
+    return subFolders(params.parentId ?? 1);
   },
 };
 
@@ -31,3 +40,18 @@ export const gqlserver = createHandler({
   schema: schema,
   rootValue: root,
 });
+
+const getFolder = async (id: string) => {
+  const folder = await Folder.findOne({ where: { id: id } });
+  return folder.toJSON();
+};
+
+const subFolders = async (parentId: string) => {
+  const folders = await Folder.findAll({ where: { parentId } });
+  return folders.map((f) => f.toJSON());
+};
+
+const subFiles = async (folderId: string) => {
+  const files = await File.findAll({ where: { folderId } });
+  return files.map((f) => f.toJSON());
+};
