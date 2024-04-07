07c4c7a5661ca2aeded1809930b4427c039219a6
Isaac <isaac@snibi.com>
2024-04-07 12:26:25 +1000

    🔇 [backend] remove CLI interactive progress bar
    This doesn't work on Non-TTY terminals, plus the front end now displays it nicely

--- src/filesystem/fileQueue.ts
+++ src/filesystem/fileQueue.ts
@@ -1,18 +1,11 @@
 import { addFile } from './events/addFile';
 import { addFolder } from './events/addFolder';
 import { removeFolder } from './events/removeFolder';
-import { Presets, SingleBar } from 'cli-progress';
 import { Task } from '../../graphql-types';
 import { generateAllThumbs } from '../helpers/thumbnailGenerator';
 import File from '../models/File';
 
 type QueueAction = 'addDir' | 'unlinkDir' | 'add' | 'generateThumbnails';
-
-const progressBarOptions = {}; //{ noTTYOutput: true, notTTYSchedule: 500 };
-const queueProgressBar = new SingleBar(
-  progressBarOptions,
-  Presets.shades_classic,
-);
 
 let queue = null;
 let queueDone = 0;
@@ -35,11 +28,6 @@ export const addToQueue = (
     queueDone = 0;
     queue = processQueue(action, filePath);
   }
-  queueProgressBar.start(queueTotal, queueDone);
-
-  // console.log(
-  //   `${queueCount} : ${priority ? 'Fast' : ''}Queued ${action} on ${filePath}`,
-  // );
 };
 
 const processQueue = async (action: QueueAction, filePath: string) => {
@@ -58,11 +46,9 @@ const processQueue = async (action: QueueAction, filePath: string) => {
     await generateAllThumbs(file);
   }
   queueDone++;
-  queueProgressBar.update(queueDone);
   if (queueDone == queueTotal) {
     queueDone = 0;
     queueTotal = 0;
-    queueProgressBar.stop();
   }
 };
 
