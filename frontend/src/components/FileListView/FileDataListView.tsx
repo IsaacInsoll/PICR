0e125baf6d7cd3ec2d9d2ff34cf65efcac1e0a8c
Isaac <isaac@snibi.com>
2024-07-28 13:24:54 +1000

    ♻️ PicrDataGrid refactor

--- frontend/src/components/FileListView/FileDataListView.tsx
+++ frontend/src/components/FileListView/FileDataListView.tsx
@@ -1,33 +1,25 @@
-import { useMemo } from 'react';
 import { MinimalFile } from '../../../types';
 import { FileListViewStyleComponentProps } from './FolderContentsView';
 import prettyBytes from 'pretty-bytes';
-import { picrGridProps } from '../PicrDataGrid';
-import {
-  MantineReactTable,
-  MRT_ColumnDef,
-  useMantineReactTable,
-} from 'mantine-react-table';
+import { PicrColumns, PicrDataGrid } from '../PicrDataGrid';
 import { Page } from '../Page';
 
 export const FileDataListView = ({
   files,
   setSelectedFileId,
 }: FileListViewStyleComponentProps) => {
-  const tableOptions = useMemo(
-    () => picrGridProps(columns, files, (row) => setSelectedFileId(row.id)),
-    [files], //files changes whenever you change a filtering option
-  );
-  const table = useMantineReactTable(tableOptions);
-
   return (
     <Page>
-      <MantineReactTable table={table} />
+      <PicrDataGrid
+        columns={columns}
+        data={files}
+        onClick={(row) => setSelectedFileId(row.id)}
+      />
     </Page>
   );
 };
 
-const columns: MRT_ColumnDef<MinimalFile>[] = [
+const columns: PicrColumns<MinimalFile>[] = [
   { accessorKey: 'name', header: 'Name' },
   { accessorKey: 'type', header: 'Type' },
   {
