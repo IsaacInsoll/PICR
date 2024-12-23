6ad55c9b63e644adba530d18f1adc7e8ff4684f6
Isaac <isaac@snibi.com>
2024-12-23 13:00:55 +1000

    🚧 [wip] drizzle - migrating Folder (partly done v2)

--- backend/db/picrDb.ts
+++ backend/db/picrDb.ts
@@ -1,15 +1,24 @@
 // This is just convenience functions and types because sometimes Drizzle is a bit too low level
 
-import { folderTable } from './models';
+import { fileTable, folderTable } from './models';
 import { db } from '../server';
 
 export type DBFolder = typeof folderTable.$inferSelect;
+export type DBFile = typeof fileTable.$inferSelect;
 
 export const DBFolderForId = async (
   id: string | number | undefined,
 ): Promise<DBFolder | undefined> => {
   if (!id) return undefined;
   return db.query.folderTable.findFirst({
+    where: (f, { eq }) => eq(f.id, id) && eq(f.exists, true),
+  });
+};
+export const DBFileForId = async (
+  id: string | number | undefined,
+): Promise<DBFile | undefined> => {
+  if (!id) return undefined;
+  return db.query.fileTable.findFirst({
     where: (f, { eq }) => eq(f.id, id) && eq(f.exists, true),
   });
 };
--- backend/graphql/helpers/getFolder.ts
+++ backend/graphql/helpers/getFolder.ts
@@ -7,7 +7,7 @@ import { DBFolderForId } from '../../db/picrDb';
 
 export const getFolder = async (id: string | number) => {
   const folder = await DBFolderForId(id);
-  const data = folder.toJSON();
+  const data = { ...folder };
   data.heroImage = await heroImageForFolder(folder);
   data.subFolders = await subFolders(id);
   data.files = await subFiles(id);
