20fbd66e436c6ff7b6f388ad9dacf68a7402fb39
Isaac <isaac@snibi.com>
2024-12-30 16:48:16 +1000

    🚸 Comment Modal: autofocus on text input field
    plus typescript bugfix

--- backend/graphql/gqlServer.ts
+++ backend/graphql/gqlServer.ts
@@ -4,11 +4,12 @@ import { IncomingCustomHeaders } from '../types/incomingCustomHeaders';
 
 export const gqlServer = createHandler({
   schema: schema,
-  context: async (req, params): Promise<IncomingCustomHeaders> => {
+  context: async (req, params) => {
     const headers = req.headers as IncomingCustomHeaders;
 
     // `req.raw.ip` works fine unless we are behind a reverse proxy
-    const ipAddress =
+    // @ts-ignore it has always been a string (not string[]) so far so i'm not overcomplicating it... yet
+    const ipAddress: string =
       headers['x-real-ip'] ?? headers['x-forwarded-for'] ?? req.raw.ip ?? '';
 
     return {
