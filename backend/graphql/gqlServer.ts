d0b954adbb5bd9eba957fbb8701ccd12f521383b
Isaac <isaac@snibi.com>
2024-12-30 14:02:13 +1000

    👽️ AccessLogs - correctly get IP address from reverse proxy

--- backend/graphql/gqlServer.ts
+++ backend/graphql/gqlServer.ts
@@ -4,16 +4,20 @@ import { IncomingCustomHeaders } from '../types/incomingCustomHeaders';
 
 export const gqlServer = createHandler({
   schema: schema,
-  context: async (req, params) => {
+  context: async (req, params): Promise<IncomingCustomHeaders> => {
     const headers = req.headers as IncomingCustomHeaders;
+
+    // `req.raw.ip` works fine unless we are behind a reverse proxy
+    const ipAddress =
+      headers['x-real-ip'] ?? headers['x-forwarded-for'] ?? req.raw.ip ?? '';
 
     return {
       auth: headers.authorization ?? '',
       uuid: headers.uuid,
       host: headers.host,
-      sessionId: headers.sessionid, //note: header field is lower case
-      ipAddress: req.raw.ip ?? '',
+      sessionId: headers.sessionid as string, //note: header field is lower case
       userAgent: headers['user-agent'],
+      ipAddress,
     };
   },
 });
