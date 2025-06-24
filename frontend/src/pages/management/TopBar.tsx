aa1a1ff56f3ef8fe4eeb92350d7eaeaded27fd55
Isaac Insoll <isaac@thebos.com.au>
2025-06-24 12:51:29 +1000

    💄 [frontend] logo cleanup

--- frontend/src/pages/management/TopBar.tsx
+++ frontend/src/pages/management/TopBar.tsx
@@ -5,7 +5,7 @@ import { PicrTitle } from '../../components/PicrTitle';
 export const TopBar = ({ title }: { title: string }) => {
   return (
     <Group pt="lg" pb="sm">
-      <PicrLogo style={{ height: 32 }} />
+      <PicrLogo style={{ height: 32, width: 32 }} />
       <Title>{title}</Title>
       <PicrTitle title={title} />
     </Group>
