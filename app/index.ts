e86c8c4911c088e22e80cda88073ad16d29e791c
IsaacInsoll <isaac@snibi.com>
2025-07-10 20:43:34 +1000

    ➕ [app] add unistyles

---
+++ app/index.ts
@@ -0,0 +1,2 @@
+import 'expo-router/entry';
+import './src/unistyles.ts';
---
+++ app/src/unistyles.ts
@@ -0,0 +1,48 @@
+import { StyleSheet } from 'react-native-unistyles';
+
+const lightTheme = {
+  colors: {
+    primary: '#ff1ff4',
+    secondary: '#1ff4ff',
+  },
+  // gap: (v: number) => v * 8,
+};
+
+const otherTheme = {
+  colors: {
+    primary: '#aa12ff',
+    secondary: 'pink',
+  },
+  // gap: (v: number) => v * 8,
+};
+
+const appThemes = {
+  light: lightTheme,
+  other: otherTheme,
+};
+
+const breakpoints = {
+  xs: 0,
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+  superLarge: 2000,
+  tvLike: 4000,
+};
+
+type AppBreakpoints = typeof breakpoints;
+type AppThemes = typeof appThemes;
+
+declare module 'react-native-unistyles' {
+  export interface UnistylesThemes extends AppThemes {}
+  export interface UnistylesBreakpoints extends AppBreakpoints {}
+}
+
+StyleSheet.configure({
+  settings: {
+    initialTheme: 'light',
+  },
+  breakpoints,
+  themes: appThemes,
+});
