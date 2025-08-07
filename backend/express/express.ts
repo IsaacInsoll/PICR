6e0c75bf051925707daa4b80bd7a2aee59509e0c
Isaac <isaac@snibi.com>
2025-08-07 21:00:06 +1000

    🚧 [backend] add debug endpoint

--- backend/express/express.ts
+++ backend/express/express.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { gqlServer } from "../graphql/gqlServer.js";
 import { imageRequest } from "./imageRequest.js";
 import { zipRequest } from "./zipRequest.js";
@@ -11,9 +11,16 @@ export const expressServer = () => {
   exp.use(express.static('public', { index: false }));
   exp.get('/image/:id/:size/:hash/:filename', imageRequest); //filename is ignored but nice for users to see a 'nice' name
   exp.get('/zip/:folderId/:hash/:filename', zipRequest); //filename is ignored but nice for users to see a 'nice' name
-
+  exp.get('/debug',debug);
   //catch all other URLS and return the front end template
   exp.use(picrTemplate);
 
   return exp;
+};
+
+const debug=async (
+  req: Request,
+  res: Response) =>{
+  console.log(req);
+  res.sendStatus(404);
 };
