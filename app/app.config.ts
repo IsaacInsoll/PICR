d7c5e33a994399033b3cbb48207806d07d21219d
Isaac Insoll <isaac@snibi.com>
2025-12-16 11:04:52 +1000

    🚀 [app] Release 1.0.3

--- app/app.config.ts
+++ app/app.config.ts
@@ -16,7 +16,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: appName,
   slug: 'picr',
-  version: '1.0.2',
+  version: '1.0.3',
   orientation: 'default',
   icon: icon,
   scheme: scheme,
