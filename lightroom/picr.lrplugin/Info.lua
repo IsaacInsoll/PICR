6d5b85ae22ed702fa0d9a516608e70d553471fa9
Isaac <isaac@snibi.com>
2026-02-02 18:34:58 +1000

    🔌 Lightroom plugin v0.3.0

--- lightroom/picr.lrplugin/Info.lua
+++ lightroom/picr.lrplugin/Info.lua
@@ -8,8 +8,8 @@ return {
   LrPluginInfoUrl = "https://github.com/IsaacInsoll/PICR/tree/master/lightroom",
   VERSION = {
     major = 0,
-    minor = 2,
-    revision = 1,
+    minor = 3,
+    revision = 0,
   },
   LrLibraryMenuItems = {
     {
