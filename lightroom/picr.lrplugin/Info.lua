545e5cde532601467353cd54f504d7673e1c564a
Isaac <isaac@snibi.com>
2026-07-06 14:19:14 +1000

    🚀 Release 1.1.0

--- lightroom/picr.lrplugin/Info.lua
+++ lightroom/picr.lrplugin/Info.lua
@@ -8,8 +8,8 @@ return {
   LrPluginInfoUrl = "https://github.com/IsaacInsoll/PICR/tree/master/lightroom",
   VERSION = {
     major = 1,
-    minor = 0,
-    revision = 2,
+    minor = 1,
+    revision = 0,
   },
   LrLibraryMenuItems = {
     {
