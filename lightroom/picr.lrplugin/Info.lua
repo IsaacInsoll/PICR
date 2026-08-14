895330c83040cc0ccba10632ff9ee26ddd5fac0a
Isaac Insoll <isaac@thebos.com.au>
2026-08-14 14:16:29 +1000

    🚀 Release 1.4.0

--- lightroom/picr.lrplugin/Info.lua
+++ lightroom/picr.lrplugin/Info.lua
@@ -8,8 +8,8 @@ return {
   LrPluginInfoUrl = "https://github.com/IsaacInsoll/PICR/tree/master/lightroom",
   VERSION = {
     major = 1,
-    minor = 3,
-    revision = 7,
+    minor = 4,
+    revision = 0,
   },
   LrLibraryMenuItems = {
     {
